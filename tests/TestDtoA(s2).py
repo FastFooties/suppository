35421abb63e3b1d4f55d73088d24a40fcef87847
Maurits van Mastrigt <maurits@nerdieworks.nl>
2016-05-23 22:42:30 +0200

    Fix TestDtoA(s2) - part 3.

--- tests/TestDtoA(s2).py
+++ tests/TestDtoA(s2).py
@@ -7,49 +7,55 @@ from src.fcfs import FCFS
 from src.rr import RR
 from src.cgc import CGC
 
+np.random.seed(3)       # Random number generator
+
 """Server departures the arrivals for the next server"""
 Distribution = 'p'
 Q = [[0] * 0, [0] * 0, [0] * 0]
 N = 2
 LD = [[0] * 10, [0] * 10, [0] * 10]
-AI =[5,5,5]
+AI = [5, 5, 5]
 I = 3
 C = 25
 S = 2
-
-# Determine arrivals
-A = [0] * I
-if Distribution == 'p':
-    for i in range(I):
-        A[i] = np.random.poisson(AI[i])
-else:
-    for i in range(I):
-        A[i] = AI[i]
 
 FFS = []
 RRS = []
 CGCS = []
 
 for i in range(S):
-    FFS .append(Server(C, i))
-    RRS .append(Server(C, i))
-    CGCS.append(Server(C, i))
+	FFS .append(Server(C, I))
+	RRS .append(Server(C, I))
+	CGCS.append(Server(C, I))
+
+FFS[0] .Q = list(Q)
+RRS[0] .Q = list(Q)
+CGCS[0].Q = list(Q)
 
 for n in range(N):
-    Af = list(A)
-    Ar = list(A)
-    Ac = list(A)
+	# Determine arrivals
+	A = [0] * I
+	if Distribution == 'p':
+		for i in range(I):
+			A[i] = np.random.poisson(AI[i])
+	else:
+		for i in range(I):
+			A[i] = AI[i]
 
-    # Servers
-    for s in range(S):
-        FCFS(FFS[s], Af, n)
-        print('FCFS','Period:', n, 'Server %s' % (s + 1) , 'Last Departures:', FFS[s].LD)
-        Af = FFS[s].LD
+	Af = list(A)
+	Ar = list(A)
+	Ac = list(A)
 
-        RR(RRS[s], Ar, n)
-        print('RR', 'Period:', n, 'Server %s' % (s + 1) , 'Last Departures:', RRS[s].LD)
-        Ar = RRS[s].LD
+	# Servers
+	for s in range(S):
+		FCFS(FFS[s], Af, n)
+		print('FCFS','Period:', n, 'Server %s' % (s + 1), 'Last Departures:', FFS[s].LD, 'Arrivals:', Af)
+		Af = FFS[s].LD
 
-        CGC(CGCS[s], Ac, n)
-        print('CGC', 'Period:', n, 'Server %s' % (s + 1), 'Last Departures:', CGCS[s].LD)
-        Ac = CGCS[s].LD
+		RR(RRS[s], Ar, n)
+		print('RR', 'Period:', n, 'Server %s' % (s + 1), 'Last Departures:', RRS[s].LD, 'Arrivals:', Ar)
+		Ar = RRS[s].LD
+
+		CGC(CGCS[s], Ac, n)
+		print('CGC', 'Period:', n, 'Server %s' % (s + 1), 'Last Departures:', CGCS[s].LD, 'Arrivals:', Ac)
+		Ac = CGCS[s].LD
